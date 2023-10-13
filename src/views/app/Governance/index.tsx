b97317b6d7bc719341abc9158db29a31095b3215
Nzenou <nathannzenou@gmail.com>
2023-10-13 19:15:42 +0200

    last fixes, still need to put step modal everywhere, build done

--- src/views/app/Governance/index.tsx
+++ src/views/app/Governance/index.tsx
@@ -10,8 +10,8 @@ function Governance() {
   return (
     <div className='mt-10'>
      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
-        <TotalSpent name="utilization/cap vs time" percentage="2.45%" />
-        <TotalSpent name="Earning vs time" percentage="2.45%" />
+        {/* <TotalSpent name="utilization/cap vs time" percentage="2.45%" /> */}
+        {/* <TotalSpent name="Earning vs time" percentage="2.45%"  /> */}
       </div>
       {/* <Card extra="order-4" >
           <div className="  rounded-xl">

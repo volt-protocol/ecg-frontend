b841da7753ab41b0185d0eb679f3bc7ba9e5c7cc
Alfu <Alfulinku@gmail.com>
2024-04-16 18:16:10 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export const runtime = 'nodejs';
 export const dynamic = 'force-static';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  
   return (
     <html lang="en" className={inter.className}>
       <body id={'root'}>

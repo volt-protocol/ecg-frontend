0e009a8e8da8eeb603d7cef374c9a6388592fbf3
Erwan Beauvois <eswak@users.noreply.github.com>
2024-04-20 19:56:16 +0200

    Fix input with decimals

--- src/app/(app)/lending/details/components/StakeCredit/index.tsx
+++ src/app/(app)/lending/details/components/StakeCredit/index.tsx
@@ -80,7 +80,7 @@ function StakeCredit({
     const inputValue = e.target.value;
 
     // Vérifier si la valeur saisie ne contient que des numéros
-    if (/^\d*$/.test(inputValue)) {
+    if (/^[0-9]*\.?[0-9]*$/i.test(inputValue)) {
       setValue(inputValue as string);
 
       await getDebtCeilingDelta(inputValue);

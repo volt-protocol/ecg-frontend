5f6ed9908ebd9cc2a1616dd9d8b940ce96b4765f
Erwan Beauvois <eswak@users.noreply.github.com>
2024-08-13 20:01:00 +0200

    Fix VoteStatusBar rounding error

--- src/components/bar/VoteStatusBar.tsx
+++ src/components/bar/VoteStatusBar.tsx
@@ -2,24 +2,18 @@ import clsx from 'clsx';
 
 export default function VoteStatusBar({ width, height, votes }: { width: number; height: number; votes: BigInt[] }) {
   const totalVotes = votes.reduce((acc, vote) => acc + Number(vote), 0);
-  const againstVotes = Math.round((Number(votes[0]) / totalVotes) * 100);
-  const forVotes = Math.round((Number(votes[1]) / totalVotes) * 100);
-  const abstainVotes = Math.round((Number(votes[2]) / totalVotes) * 100);
+  const againstVotes = Math.ceil((Number(votes[0]) / totalVotes) * 100);
+  const forVotes = Math.ceil((Number(votes[1]) / totalVotes) * 100);
+  const abstainVotes = Math.max(0, 100 - againstVotes - forVotes);
 
   return (
-    <div style={{ width: `${width}px`, height: `${height}px` }} className={`flex h-8`}>
-      <div
-        style={{ width: `${(forVotes / 100) * width}px` }}
-        className={clsx(forVotes == 100 ? 'rounded-md' : 'rounded-l-md', `w-1/3  bg-green-400`)}
-      ></div>
-      <div
-        style={{ width: `${(abstainVotes / 100) * width}px` }}
-        className={clsx(abstainVotes == 100 && 'rounded-md', `w-1/3  bg-gray-400`)}
-      ></div>
-      <div
-        style={{ width: `${(againstVotes / 100) * width}px` }}
-        className={clsx(againstVotes == 100 ? 'rounded-md' : 'rounded-r-md', `w-1/3  bg-red-400`)}
-      ></div>
+    <div
+      style={{ width: `${width - 1}px`, height: `${height}px`, borderRadius: `${height}px` }}
+      className={`flex h-8 overflow-hidden`}
+    >
+      <div style={{ width: `${(forVotes / 100) * width}px` }} className={clsx(`w-1/3  bg-green-400`)}></div>
+      <div style={{ width: `${(abstainVotes / 100) * width}px` }} className={clsx(`w-1/3  bg-gray-400`)}></div>
+      <div style={{ width: `${(againstVotes / 100) * width}px` }} className={clsx(`w-1/3  bg-red-400`)}></div>
     </div>
   );
 }
